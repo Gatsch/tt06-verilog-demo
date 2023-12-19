ba435c6bd301e5a1357d60ff9b1b2a4230cf1564
Gatsch <magarstenauer@gmail.com>
2023-12-19 14:46:00 +0100

    fixed tb.v

--- src/tb.v
+++ src/tb.v
@@ -1,27 +1,47 @@
-`timescale 1ns/1ns
-`include "tt_um_counter.v"
+`default_nettype none
+`timescale 1ns/1ps
 
-module tb;
+/*
+this testbench just instantiates the module and makes some convenient wires
+that can be driven / tested by the cocotb test.py
+*/
 
-	parameter BW = 3;
-	reg rst_i = 1'b1;
-	reg clk_i = 1'b0;
-	wire [BW-1:0] cnt_val;
-	
-	tt_um_i2c 
-		#()
-		counter_dut (
-			.clk_i(clk_i),
-			.rst_i(rst_i),
-			.counter_val_o(cnt_val)
-		);
-		
-	always #5 clk_i  = ~clk_i;
-	
-	initial begin 
-		$dumpfile("tb.vcd");
-		$dumpvars;
-		#50 rst_i = 1'b0;
-		#200 $finish;
-	end
+// testbench is controlled by test.py
+module tb ();
+
+    // this part dumps the trace to a vcd file that can be viewed with GTKWave
+    initial begin
+        $dumpfile ("tb.vcd");
+        $dumpvars (0, tb);
+        #1;
+    end
+
+    // wire up the inputs and outputs
+    reg  clk;
+    reg  rst_n;
+    reg  ena;
+    reg  [7:0] ui_in;
+    reg  [7:0] uio_in;
+
+    wire [6:0] segments = uo_out[6:0];
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
+
+    tt_um_i2c tt_um_i2c (
+    // include power ports for the Gate Level test
+    `ifdef GL_TEST
+        .VPWR( 1'b1),
+        .VGND( 1'b0),
+    `endif
+        .ui_in      (ui_in),    // Dedicated inputs
+        .uo_out     (uo_out),   // Dedicated outputs
+        .uio_in     (uio_in),   // IOs: Input path
+        .uio_out    (uio_out),  // IOs: Output path
+        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena        (ena),      // enable - goes high when design is selected
+        .clk        (clk),      // clock
+        .rst_n      (rst_n)     // not reset
+        );
+
 endmodule
